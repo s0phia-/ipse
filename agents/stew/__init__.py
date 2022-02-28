fc1c5fd427535c01a4a49402c14d4f966c016019
s0phia- <sophiajones@live.co.uk>
2022-02-28 15:04:08 +0000

    Run files for evaluating and learning with a DQN-style agent

--- agents/stew/__init__.py
+++ agents/stew/__init__.py
@@ -1,3 +1,3 @@
-#from .mlogit import StewMultinomialLogit
-#from .choice_set_data import ChoiceSetData
+from .mlogit import StewMultinomialLogit
+from .choice_set_data import ChoiceSetData
 # from .domfilter import inner_loop
