9dc3bbd7b4e6d337a3cd5c147661577f39b493ad
s0phia- <sophiajones@live.co.uk>
2022-02-28 16:28:50 +0000

    Plot from dictionary, to enable comparing different runs

--- dqn_style_run_files/plot.py
+++ dqn_style_run_files/plot.py
@@ -1,5 +1,12 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 
-def plot_dict():
-    pass
+def plot_dict(returns_dict, x_axis_factor, legend_label):
+    for key, returns in returns_dict.items():
+        x_axis = np.arange(0, len(returns)*x_axis_factor, x_axis_factor)
+        plt.plot(x_axis, returns, label=key)
+    plt.legend(title=legend_label)
+    plt.xlabel("Episodes")
+    plt.ylabel("Total Return")
+    plt.show()
