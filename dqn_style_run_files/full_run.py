d4d16fe188812cd6c73165505bad84a995bfbd7f
s0phia- <sophiajones@live.co.uk>
2022-03-31 10:05:57 +0100

    Clearer file labelling

--- dqn_style_run_files/full_run.py
+++ dqn_style_run_files/full_run.py
@@ -8,7 +8,7 @@ from agents.QEW_v2 import QEWv2
 def full_run(i_agent, evaluate_every_x_episodes, eval_iterations, sleep, max_ep_len, num_episodes, reg_coef, agent,
              df, results_path):
     env = CartPoleRBF(direct_features=df)
-    label = agent + str(reg_coef) + str(df) + str(i_agent)
+    label = agent + '_' + str(reg_coef) + '_' + str(df) + '_' + str(i_agent)
     agent = QEWv2(num_features=env.num_features, actions=env.action_space,
                   regularisation_strength=reg_coef, model=agent)
     returns = control_evaluation(agent, env, sleep, num_episodes, max_ep_len, evaluate_every_x_episodes,
