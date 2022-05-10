588704a38ada29ccf1f07faacabae76833fe9626
s0phia- <sophiajones@live.co.uk>
2022-05-10 21:25:31 +0100

    Add final reward table

--- analysis/run_analysis.py
+++ analysis/run_analysis.py
@@ -2,7 +2,7 @@ from analysis.gather_process_data import get_data, process_data, find_best_reg_c
 from analysis.plot import plot_gg, plot_coef_return
 
 eval_every_x_episodes = 3
-folder_path = '../results/hello'
+folder_path = '../results/hex2'
 
 all_returns = get_data(folder_path, eval_every_x_episodes)
 all_returns = process_data(all_returns)
@@ -13,10 +13,13 @@ fig.savefig('image.png', dpi=300, bbox_inches='tight')
 #####################################################
 #  For finding the best regularisation coefficient  #
 #####################################################
-#
+
 avg_returns = find_best_reg_coef(all_returns)
 plot, fig = plot_coef_return(avg_returns)
 fig.savefig('reg_image.png', dpi=300, bbox_inches='tight')
 
 best = avg_returns.loc[avg_returns.groupby(by='agent')['return'].idxmax()]
 print(best)
+
+hello = all_returns[all_returns['episode'] == max(all_returns['episode'])].groupby(by='agent')['return'].idxmax()
+print(all_returns.loc[hello])
