5ae74b2343b9780c928aa599ea245b0e7b34eea7
s0phia- <sophiajones@live.co.uk>
2022-05-10 11:02:58 +0100

    Remove a file

--- dqn_style_run_files/evaluate.py
+++ dqn_style_run_files/evaluate.py
@@ -1,5 +1,11 @@
 import time
 import statistics
+import numpy as np
+from envs.cartpole_rbf import CartPoleRBF
+from agents.QEW_v2 import RidgeAgent, PureEwAgent, StewAgent, LinRegAgent
+from agents.QEW import QStewAgentType1, QRidgeAgentType1, QEwAgentType1, QLinRegType1
+from agents.LSPI import LspiAgent, LspiAgentEw
+# from agents.NN_QEW import DQNAgent
 
 
 def evaluate(agent, env, sleep_time, episodes, max_episode_length):
@@ -35,3 +41,16 @@ def control_evaluation(agent, env, sleep_time, num_episodes, max_episode_length,
         agent.run(env=env, episodes=evaluate_every_x_episodes, max_episode_length=max_episode_length,
                   sleep_time=sleep_time, stopping_criteria=stopping_criteria)
     return all_returns
+
+
+def full_run(i_agent, evaluate_every_x_episodes, eval_iterations, sleep, max_ep_len, num_episodes, reg_coef, agent_name,
+             direct_features, results_path, stopping_criteria=.005):
+    env = CartPoleRBF(direct_features=direct_features)
+    label = agent_name + '_' + str(reg_coef) + '_' + str(direct_features) + '_' + str(i_agent)
+    print(label)
+    agent = globals()[agent_name]
+    agent = agent(env.num_features, env.action_space, regularisation_strength=reg_coef)
+    returns = control_evaluation(agent, env, sleep, num_episodes, max_ep_len, evaluate_every_x_episodes,
+                                 eval_iterations, stopping_criteria)
+    np.save(results_path + '/' + label, returns)
+    return returns
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import numpy as np
 import multiprocessing as mp
 import argparse
 
-from dqn_style_run_files.full_run import full_run
+from dqn_style_run_files.evaluate import full_run
 
 
 if __name__ == '__main__':
@@ -13,19 +13,19 @@ if __name__ == '__main__':
     if not os.path.exists(results_path):
         os.makedirs(results_path)
 
-    pool = mp.Pool(1)#mp.cpu_count())
+    pool = mp.Pool(mp.cpu_count())
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--num_agents', type=int, default=3)
+    parser.add_argument('--num_agents', type=int, default=1)
     parser.add_argument('--eval_every_x_episodes', type=int, default=5)
     parser.add_argument('--eval_iterations', type=int, default=3)
-    parser.add_argument('--sleep', type=int, default=0.1)
+    parser.add_argument('--sleep', type=int, default=0)
     parser.add_argument('--max_ep_len', type=int, default=200)
     parser.add_argument('--episodes', type=int, default=150)
-    parser.add_argument('--reg_strengths', type=list, default=np.logspace(-2, 4, 40))  # 2500, 30ew
-    parser.add_argument('--agents', type=list, default=["RidgeAgent", "PureEwAgent", "StewAgent", "LinRegAgent",
-                                                        "QStewAgentType1", "QRidgeAgentType1", "QEwAgentType1",
-                                                        "QLinRegType1", "LspiAgent", "LspiAgentEw"])
+    parser.add_argument('--reg_strengths', type=list, default=np.logspace(-2, 4, 10))  # 2500, 30ew
+    parser.add_argument('--agents', type=list, default=["LspiAgentEw"])#, "LspiAgent"])  #"RidgeAgent", "PureEwAgent", "StewAgent", "LinRegAgent"])#,
+                                                    #    "QStewAgentType1", "QRidgeAgentType1", "QEwAgentType1",
+                                                    #    "QLinRegType1", "LspiAgent", "LspiAgentEw"])
     parser.add_argument('--direct_features', type=list, default=[False])
     args = parser.parse_args()
 
@@ -35,6 +35,9 @@ if __name__ == '__main__':
                     for reg_coef in args.reg_strengths
                     for agent in args.agents
                     for df in args.direct_features]
-    # all_run_args = [[agent_i, 3, 3, 0, 200, 150, 30, "stew", False, results_path] for agent_i in range(30)] + \
-    #                [[agent_i, 3, 3, 0, 200, 150, 14, "ridge", False, results_path] for agent_i in range(30)]
+    # all_run_args = [[agent_i, 3, 3, 0, 200, 150, 4.6, "StewAgent", False, results_path] for agent_i in range(30)] + \
+    #                [[agent_i, 3, 3, 0, 200, 150, 460, "RidgeAgent", False, results_path] for agent_i in range(30)] + \
+    #                [[agent_i, 3, 3, 0, 200, 150, 1, "PureEwAgent", False, results_path] for agent_i in range(30)] + \
+    #                [[agent_i, 3, 3, 0, 200, 150, 1, "LinRegAgent", False, results_path] for agent_i in range(30)]
+
     pool.starmap(full_run, all_run_args)
