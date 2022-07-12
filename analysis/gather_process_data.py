4206684d3c2b90c396013783782ed8512466bdf6
s0phia- <sophiajones@live.co.uk>
2022-07-12 19:11:41 +0200

    Remove need for a list of agents. Fix the epsiode number on graphs

--- analysis/gather_process_data.py
+++ analysis/gather_process_data.py
@@ -13,22 +13,24 @@ def get_data(folder_path, eval_every_x_episodes):
         returns = np.load(folder_path + '/' + run, allow_pickle=True)
         agent, reg_coef, directed_features, agent_number = run[:-4].split('_')
         n = len(returns)
-        returns_one_run = {'agent': [agent]*n,
-                           'reg_coef': [float(reg_coef)]*n,
-                           'directed_features': [directed_features]*n,
-                           'agent_number': [agent_number]*n,
-                           'episode': range(eval_every_x_episodes, (n+1)*eval_every_x_episodes, eval_every_x_episodes),
+        returns_one_run = {'agent': [agent] * n,
+                           'reg_coef': [float(reg_coef)] * n,
+                           'directed_features': [directed_features] * n,
+                           'agent_number': [agent_number] * n,
+                           'episode': range(eval_every_x_episodes, (n + 1) * eval_every_x_episodes,
+                                            eval_every_x_episodes),
                            'return': returns}
         returns_one_run = pd.DataFrame(returns_one_run)
         all_returns = pd.concat([all_returns, returns_one_run], axis=0)
-       #  all_returns = all_returns[all_returns['episode'] < 151]
+    #  all_returns = all_returns[all_returns['episode'] < 151]
     all_returns['reg_coef'] = all_returns['reg_coef'].astype(str)
     return all_returns
 
 
-def process_data(df, compare_agents):
-    df = df[df.agent.isin(compare_agents.keys())]
-    df = df.replace({"agent": compare_agents})
+def process_data(df, compare_agents=None):
+    if compare_agents is not None:
+        df = df[df.agent.isin(compare_agents.keys())]
+        df = df.replace({"agent": compare_agents})
     processed_data = df.groupby(['agent', 'reg_coef', 'episode'], as_index=False)['return'].agg(
         {'mean': 'mean', 'std': 'std'}).reset_index()
     processed_data['ymin'] = processed_data['mean'] - processed_data['std']
